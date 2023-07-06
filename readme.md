1334835119884d6212b9e45c4e1758a9232ed53a
fadzilahrafiq <fadzilahrafiq@gmail.com>
2023-07-06 16:26:20 +0800

    UPDATE: V0.5

--- readme.md
+++ readme.md
@@ -4,3 +4,9 @@ Credits:
 - <a href="https://www.flaticon.com/free-icons/code" title="code icons">Code icons created by Icongeek26 - Flaticon</a>
 - <a href="https://www.flaticon.com/free-icons/web-development" title="web development icons">Web development icons created by Freepik - Flaticon</a>
 - <a href="https://www.flaticon.com/free-icons/software" title="software icons">Software icons created by Eucalyp - Flaticon</a>
+- <a href="https://www.flaticon.com/free-icons/email" title="email icons">Email icons created by kmg design - Flaticon</a>
+- <a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by riajulislam - Flaticon</a>
+- <a href="https://www.flaticon.com/free-icons/whatsapp" title="whatsapp icons">Whatsapp icons created by Pixel perfect - Flaticon</a>
+- <a href="https://www.flaticon.com/free-icons/whatsapp" title="whatsapp icons">Whatsapp icons created by Pixel perfect - Flaticon</a>
+- <a href="https://www.flaticon.com/free-icons/email" title="email icons">Email icons created by Freepik - Flaticon</a>
+- <a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by Freepik - Flaticon</a>
--- script.js
+++ script.js
@@ -1,19 +1,79 @@
 window.onload = (event) => {
-    setTimeout( () => {
-      let floatingPrompt = document.getElementById("floating-scroll-prompt");
-      floatingPrompt.classList.add("hide");
-    }, 15000);
+    // setTimeout( () => {
+    //   let floatingPrompt = document.getElementById("floating-scroll-prompt");
+    //   floatingPrompt.classList.add("hide");
+    // }, 15000);
+
+    // setTimeout( () => {
+    //   document.getElementsByClassName("landing-title")[0].classList.add("reveal");
+    //   document.getElementsByClassName("landing-menu")[0].classList.add("reveal");
+    // }, 5000)
 
     const introObserver = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         // If the element is visible
         if (entry.isIntersecting) {
           // Add the animation class
-          document.getElementById("biography-intro__profile-pic").classList.add("anim-slide-in");
-          document.getElementById("biography-intro__introduction").classList.add("anim-slide-in");
+          document.getElementById("biography-intro__content").classList.add("anim-slide-in");
+          // document.getElementById("biography-intro__introduction").classList.add("anim-slide-in");
         }
       });
     });
 
-    introObserver.observe(document.querySelector('.biography-intro__introduction'));
+    // introObserver.observe(document.querySelector('.biography-intro'));
+
+    const skillObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+          // Add the animation class
+          let skillBubblesText = document.getElementsByClassName("skill-bubble__title-text");
+          let skillBubbles = document.getElementsByClassName("skill-bubble");
+          for (let i = 0 ; i < skillBubbles.length ; i++) {
+            let skillBubble = skillBubbles[i];
+            let skillBubbleText = skillBubblesText[i];
+            skillBubble.classList.add("show");
+            skillBubbleText.classList.add("show");
+          }
+        }
+      });
+    });
+
+    // skillObserver.observe(document.querySelector('.skill-display'));
   };
+
+  function scrollToBiography() {
+    document.getElementById("biography-section").scrollIntoView({ behavior: "smooth" });
+  }
+
+  function navigateToLanding() {
+    document.getElementById("landing-section").scrollIntoView({ behavior: "smooth" });
+  }
+
+  function goToLanding() {
+    document.getElementById("main-container").classList.add("fade-out");
+    setTimeout( () => {
+      window.location.href = "./index.html";
+    }, 1000)
+  }
+
+  function navigateToContact() {
+    document.getElementById("main-container").classList.add("fade-out");
+    setTimeout( () => {
+      window.location.href = "./contact.html";
+    }, 1000)
+  }
+
+  function navigateToExperience() {
+    document.getElementById("biography-section").classList.add("slide-out-left");
+    setTimeout( () => {
+      window.location.href = "./experience.html";
+    }, 1800)
+  }
+
+  function navigateToProjects() {
+    document.getElementById("biography-section").classList.add("slide-out-right");
+    setTimeout( () => {
+      window.location.href = "./project.html";
+    }, 1800)
+  }
