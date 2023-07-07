05db33869041c4ede98862058038f359ad864740
Fadzilah Rafiq <fadzilahrafiq@gmail.com>
2023-07-07 17:46:33 +0800

    UPDATE: V0.6

--- script.js
+++ script.js
@@ -1,79 +1,140 @@
 window.onload = (event) => {
-    // setTimeout( () => {
-    //   let floatingPrompt = document.getElementById("floating-scroll-prompt");
-    //   floatingPrompt.classList.add("hide");
-    // }, 15000);
+  // setTimeout( () => {
+  //   let floatingPrompt = document.getElementById("floating-scroll-prompt");
+  //   floatingPrompt.classList.add("hide");
+  // }, 15000);
 
-    // setTimeout( () => {
-    //   document.getElementsByClassName("landing-title")[0].classList.add("reveal");
-    //   document.getElementsByClassName("landing-menu")[0].classList.add("reveal");
-    // }, 5000)
+  // setTimeout( () => {
+  //   document.getElementsByClassName("landing-title")[0].classList.add("reveal");
+  //   document.getElementsByClassName("landing-menu")[0].classList.add("reveal");
+  // }, 5000)
 
-    const introObserver = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        // If the element is visible
-        if (entry.isIntersecting) {
-          // Add the animation class
-          document.getElementById("biography-intro__content").classList.add("anim-slide-in");
-          // document.getElementById("biography-intro__introduction").classList.add("anim-slide-in");
-        }
-      });
+  const introObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        document.getElementById("biography-intro__content").classList.add("anim-slide-in");
+        // document.getElementById("biography-intro__introduction").classList.add("anim-slide-in");
+      }
     });
+  });
 
-    // introObserver.observe(document.querySelector('.biography-intro'));
+  // introObserver.observe(document.querySelector('.biography-intro'));
 
-    const skillObserver = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        // If the element is visible
-        if (entry.isIntersecting) {
-          // Add the animation class
-          let skillBubblesText = document.getElementsByClassName("skill-bubble__title-text");
-          let skillBubbles = document.getElementsByClassName("skill-bubble");
-          for (let i = 0 ; i < skillBubbles.length ; i++) {
-            let skillBubble = skillBubbles[i];
-            let skillBubbleText = skillBubblesText[i];
-            skillBubble.classList.add("show");
-            skillBubbleText.classList.add("show");
-          }
+  const skillObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        let skillBubblesText = document.getElementsByClassName("skill-bubble__title-text");
+        let skillBubbles = document.getElementsByClassName("skill-bubble");
+        for (let i = 0 ; i < skillBubbles.length ; i++) {
+          let skillBubble = skillBubbles[i];
+          let skillBubbleText = skillBubblesText[i];
+          skillBubble.classList.add("show");
+          skillBubbleText.classList.add("show");
         }
-      });
+      }
     });
+  });
 
-    // skillObserver.observe(document.querySelector('.skill-display'));
-  };
+  // skillObserver.observe(document.querySelector('.skill-display'));
 
-  function scrollToBiography() {
-    document.getElementById("biography-section").scrollIntoView({ behavior: "smooth" });
-  }
+  const exp1bserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        document.getElementById("experience-one").classList.add("anim-slide-in");
+        // document.getElementById("biography-intro__introduction").classList.add("anim-slide-in");
+      }
+    });
+  });
 
-  function navigateToLanding() {
-    document.getElementById("landing-section").scrollIntoView({ behavior: "smooth" });
-  }
+  exp1bserver.observe(document.querySelector('#experience-one'));
 
-  function goToLanding() {
-    document.getElementById("main-container").classList.add("fade-out");
-    setTimeout( () => {
-      window.location.href = "./index.html";
-    }, 1000)
-  }
+  const exp2Observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        document.getElementById("experience-two").classList.add("anim-slide-in");
+        // document.getElementById("biography-intro__introduction").classList.add("anim-slide-in");
+      }
+    });
+  });
 
-  function navigateToContact() {
-    document.getElementById("main-container").classList.add("fade-out");
-    setTimeout( () => {
-      window.location.href = "./contact.html";
-    }, 1000)
-  }
+  exp2Observer.observe(document.querySelector('#experience-two'));
 
-  function navigateToExperience() {
-    document.getElementById("biography-section").classList.add("slide-out-left");
-    setTimeout( () => {
-      window.location.href = "./experience.html";
-    }, 1800)
-  }
+  const exp3Observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        document.getElementById("experience-three").classList.add("anim-slide-in");
+        // document.getElementById("biography-intro__introduction").classList.add("anim-slide-in");
+      }
+    });
+  });
 
-  function navigateToProjects() {
-    document.getElementById("biography-section").classList.add("slide-out-right");
-    setTimeout( () => {
-      window.location.href = "./project.html";
-    }, 1800)
-  }
+  exp3Observer.observe(document.querySelector('#experience-three'));
+};
+
+function scrollToBiography() {
+  document.getElementById("biography-section").scrollIntoView({ behavior: "smooth" });
+}
+
+function navigateToLanding() {
+  document.getElementById("landing-section").scrollIntoView({ behavior: "smooth" });
+}
+
+function goToLanding() {
+  document.getElementById("main-container").classList.add("fade-out");
+  setTimeout( () => {
+    window.location.href = "./index.html";
+  }, 1000)
+}
+
+function navigateToContact() {
+  document.getElementById("main-container").classList.add("fade-out");
+  setTimeout( () => {
+    window.location.href = "./contact.html";
+  }, 1000)
+}
+
+function navigateToExperience() {
+  document.getElementById("main-container").classList.remove("fade-in");
+  document.getElementById("main-container").classList.add("slide-out-left");
+  setTimeout( () => {
+    window.location.href = "./experience.html";
+  }, 1500)
+}
+
+function navigateToProjects() {
+  document.getElementById("main-container").classList.remove("fade-in");
+  document.getElementById("main-container").classList.add("slide-out-right");
+  setTimeout( () => {
+    window.location.href = "./project.html";
+  }, 1500)
+}
+
+function navigateToAboutMe() {
+  document.getElementById("main-container").classList.remove("fade-in");
+  document.getElementById("main-container").classList.add("slide-out-right");
+  setTimeout( () => {
+    window.location.href = "./index.html?goToAbout=true";
+  }, 1500)
+}
+
+function goToLinkedIn() {
+  window.open('https://my.linkedin.com/in/fadzilahrafiq','_blank');
+}
+
+function showEmail() {
+
+}
+
+function showWhatsapp() {
+  window.open('https://wa.link/e01ht0','_blank');
+}
